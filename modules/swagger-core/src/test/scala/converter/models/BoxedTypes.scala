0e0e7cf28180444b0c96c3f7c1458bb673334fdc
gbolt <gordon.bolt@cyaninc.com>
2014-11-13 17:44:48 -0500

    gbolt - added required annotation to class markup for BoxedTypes

--- modules/swagger-core/src/test/scala/converter/models/BoxedTypes.scala
+++ modules/swagger-core/src/test/scala/converter/models/BoxedTypes.scala
@@ -9,24 +9,24 @@ import scala.annotation.meta.field
 // to erasure the parameterized types are only identified as Object
 @ApiModel(description = "Options of boxed types produces an Object ref instead of correct type")
 case class BoxedTypesIssue31(stringSeq: Seq[String], stringOpt: Option[String],
-                             @(ApiModelProperty @field)(value = "Integers in a Sequence Box") intSeq: Seq[Int],
+                             @(ApiModelProperty @field)(value = "Integers in a Sequence Box", required = false) intSeq: Seq[Int],
                              @(ApiModelProperty @field)(value = "Integer in an Option Box") intOpt: Option[Int],
                              justInt: Int)
 
 // Get around the erasure by providing the dataType explicitly using the dataType common names.
 @ApiModel(description = "Options of boxed types produces an Object ref instead of correct type, but can be overcome with dataType")
 case class BoxedTypesIssue31WithDataType(stringSeq: Seq[String], stringOpt: Option[String],
-                                         @(ApiModelProperty @field)(value = "Integers in a Sequence Box", dataType = "List[int]") intSeq: Seq[Int],
+                                         @(ApiModelProperty @field)(value = "Integers in a Sequence Box", dataType = "List[int]", required = false) intSeq: Seq[Int],
                                          @(ApiModelProperty @field)(value = "Integer in an Option Box", dataType = "int") intOpt: Option[Int],
                                          justInt: Int)
 
 // Get around erasure in a parameterized generic type
 @ApiModel(description = "Standard reply containing a list of items and paging information")
 case class ListReply[T](
-                         @(ApiModelProperty @field)(value = "List of requested items (in current page if paged)") items: List[T],
-                         @(ApiModelProperty @field)(value = "Total number of items in the reply (across all pages if applicable)") total: Int,
+                         @(ApiModelProperty @field)(value = "List of requested items (in current page if paged)", required = false) items: List[T],
+                         @(ApiModelProperty @field)(value = "Total number of items in the reply (across all pages if applicable)", required = false) total: Int,
                          @(ApiModelProperty @field)(value = "Identifier used to fetch the next page of results") nextPageToken: Option[String],
-                         @(ApiModelProperty @field)(value = "Offset within the total count of results where this current items list starts") offset: Int,
+                         @(ApiModelProperty @field)(value = "Offset within the total count of results where this current items list starts", required = false) offset: Int,
                          @(ApiModelProperty @field)(value = "Limit on the number of items included in a single response page", dataType = "int") limit: Option[Int]
                          )
 
